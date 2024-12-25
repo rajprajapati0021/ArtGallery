8d69662a80203c5068839675bbb2bae090100198
Raj Prajapati <rajprajapati.bs0021@gmail.com>
2024-12-25 12:47:10 +0530

    new chagne

--- Program.cs
+++ Program.cs
@@ -28,11 +28,11 @@ try
     var app = builder.Build();
 
     // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
+    //if (app.Environment.IsDevelopment())
+    //{
         app.UseSwagger();
         app.UseSwaggerUI();
-    }
+    //}
 
     app.UseHttpsRedirection();
     app.UseCors("AllowSpecificOrigins");
